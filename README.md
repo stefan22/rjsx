a4328ea00ca5481352ba233b6bb789af3a81524d
Stefano <stefan.emailacct@gmail.com>
2018-12-10 20:55:41 +0000

    [initial-setup] using CRAPP and my settings

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App]
+------------------------------------
+
+- hiding some files with .vscode
+
+
+
+(https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/logo.svg';
+import '../scss/App.scss';
 
 class App extends Component {
   render() {
@@ -9,16 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            this is logo
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
       </div>
     );
